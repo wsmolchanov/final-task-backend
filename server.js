92c92f8f9f3afedf9b45d28687ef4860afd4883c
wsmolchanov <ws.molchanov@gmail.com>
2019-09-17 20:16:38 +0300

    connect mongoose

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+var express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/config');
 
+var app = express();
 
-let app = express();
-
+const db = mongoose.createConnection('mongodb://localhost/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+db.on('error', function (err) {
+  if (err) {
+    console.log('MainDB');
+    console.log(err);
+  }
+});
+db.once('open', function callback () {
+  console.info('CLIENT DB connected successfully');
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(config.PORT, function () {
+  console.log(`Example app listening on port ${config.PORT}!`);
 });
