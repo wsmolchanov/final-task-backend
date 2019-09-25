05918e401e34574ee849c5995caf67e150590fa2
wsmolchanov <ws.molchanov@gmail.com>
2019-09-25 19:26:28 +0300

    add createuser

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,29 +1,36 @@
 (function () {
 	'use strict';
 
-	// const server = require('../../server');
-	// const config = require('../../config/config');
+	const server = require('../server');
+	const config = require('../config/config');
 
 	module.exports = {
 		login,
-		register
+		signup
   };
 
   function login(req, res, next) {
     console.log('login auth controller');
 
+    
     console.log(req.body);
 
     res.status(200).send({ message: 'login is good' });
   }
 
-  function register(req, res, next) {
-    console.log('register auth controller');
+  function signup(req, res, next) {
+    console.log('signup auth controller');
 
     console.log(req.body);
 
     res.status(200).send({ message: 'signup is good' });
+    let payload = {
+			...req.body,
+		}
+
   }
+
+  
 
 
 
